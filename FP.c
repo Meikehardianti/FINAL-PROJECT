8563282ce57ac0160f79b3c96b3002072534af88
dwinurhidayah <dwinurhidayah899@gmail.com>
2019-12-12 13:16:16 +0700

    update sort + update data

--- FP.c
+++ FP.c
@@ -52,6 +52,17 @@ void view(int x){
 	    printf("\ntanggal masuk		: %d-%d-%d\n",dt[x].tgl.tanggal,dt[x].tgl.bulan,dt[x].tgl.tahun);
 }
 
+void update(int x){
+		printf("Ubah Id_Pasien		: ");fflush(stdin); scanf("%d",&dt[x].pas.id_pasien);
+	    printf("Ubah Nama Pasien	: ");fflush(stdin); gets(dt[x].pas.nama);
+	    printf("Ubah Jenis Penyakit	: ");fflush(stdin); gets(dt[x].pas.jenis_penyakit);
+		printf("Ubah Jenis Kamar	: ");fflush(stdin); scanf("%s",&dt[x].jenis_kamar);
+		printf("Ubah Nomor Kamar	: ");fflush(stdin); scanf("%d",&dt[x].no_kamar);
+		printf("Ubah Tanggal Masuk	: ");fflush(stdin); scanf("%d",&dt[x].tgl.tanggal);
+		printf("Ubah Bulan Masuk	: ");fflush(stdin); scanf("%d",&dt[x].tgl.bulan);
+		printf("Ubah Bulan Masuk	: ");fflush(stdin); scanf("%d",&dt[x].tgl.tahun);
+    }
+
 int jumpsearch_idpasien(dat dt[], int size, int cari){
 	int i;
 	int mulai =0;
@@ -115,6 +126,102 @@ int jumpsearch_nokamar(dat dt[],int size,int cari){ //tidak bisa
 	}
 }
 
+void insertionsortnama(	dat arr[],int n){
+	int i, j;
+    dat key;
+    for(i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while((j >= 0) && (strcmp(arr[j+1].pas.nama, arr[j].pas.nama)<0)){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+	
+}
+
+void insertionshortid_pasien(dat arr[],int n){
+	int i, j;
+    dat key;
+    for(i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while((j >= 0) && (arr[j+1].pas.id_pasien, arr[j].pas.id_pasien)<0){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void insertionshortno_kamar(dat arr[],int n){
+	int i, j;
+    dat key;
+    for(i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while((j >= 0) && (arr[j+1].no_kamar, arr[j].no_kamar)<0){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void insertionsortj_kamar(dat arr[],int n){
+	int i, j;
+    dat key;
+    for(i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while((j >= 0) && (strcmp(arr[j+1].jenis_kamar, arr[j].jenis_kamar)<0)){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+
+}
+
+void insertionsortj_penyakit(dat arr [],int n){
+	int i,j;
+	dat key;
+	for(i=1; i<n; i++){
+		key = arr[i];
+		j = i -1;
+		while ((j >= 0) && (strcmp(arr[j+1].pas.jenis_penyakit, arr[j].pas.jenis_penyakit) < 0)){
+			dt[j+1] = dt[j];
+			j = j-1;
+		}
+		dt[j+1]=key;
+	}
+}
+
+void insertionsorttgl_masuk(dat arr [],int n){
+    int i, j;
+    dat key;
+
+    for(i = 1; i < n; i++){
+        key = arr[i];
+        for (j = i - 1; (j >= 0) && (arr[j].tgl.tahun > arr[j+1].tgl.tahun); j--){
+            arr[j + 1] = arr[j];
+            	for (j = i - 1; (j >= 0) && (arr[j].tgl.tahun == arr[j+1].tgl.tahun) && (arr[j].tgl.bulan > arr[j+1].tgl.bulan); j--){
+            		arr[j + 1] = arr[j];
+        		 	    for (j = i - 1; (j >= 0) && (arr[j].tgl.tahun == arr[j+1].tgl.tahun) && (arr[j].tgl.bulan == arr[j+1].tgl.bulan) && (arr[j].tgl.tanggal > arr[j+1].tgl.tanggal); j--){
+         				   arr[j + 1] = arr[j];
+       					}
+        		}
+        }
+        arr[j + 1] = key;
+    }
+}
+
+
 int main(){
 		
 	int i,j,menu,search,sort,n=0,gap,temp,ubah;
@@ -124,7 +231,7 @@ int main(){
 	void pilihan_menu(){
 		system("color F4");
 		printf("\n ==================================================================");
-		printf("\n \t\t\t\     PROGRAM APLIKASI");
+		printf("\n\t\t\t     PROGRAM APLIKASI");
 		printf("\n\t\t\t PENDATAAN RUMAH SAKIT");
 		printf("\n ==================================================================");
 		printf("\n\n\t\t\t1. Input Data Pasien");
@@ -145,23 +252,37 @@ int main(){
     	printf("\t3. Pencarian Berdasarkan Nomor Kamar \n");
     	printf("\t4. Kembali ke Menu Utama");
 		printf("\nPilih Menu : "); scanf("%d",&search);
+	}
+	void pilihan_sort(){
+		printf("\n ================================================");
+		printf("\n\t\tSORT DATA PASIEN\n");
+		printf("\n ================================================\n");
+		printf("\t1. Pengurutan Berdasarkan Nama Pasien \n");
+    	printf("\t2. Pengurutan Berdasarkan ID Pasien \n");
+   		printf("\t3. Pengurutan Berdasarkan Jenis Penyakit \n");
+   		printf("\t4. Pengurutan Berdasarkan Jenis Kamar \n");
+   		printf("\t5. Pengurutan Berdasarkan Nomor Kamar \n");
+		printf("\t6. Pengurutan Berdasarkan Tanggal Masuk \n");
+		printf("\t7. Kembali ke Menu Utama");
+		printf("\nPilih Menu : "); scanf("%d",&sort);
+		system("cls");
 	}	
 	do{
 		system("cls");
 		pilihan_menu();
 		if(menu==1){
-			printf("----------------------------------------\n");
-			printf("           INPUT DATA PASIEN	\n");
-			printf("----------------------------------------\n");
+			printf("\t========================================\n");
+			printf("\t           INPUT DATA PASIEN	\n");
+			printf("\t========================================\n");
 			input(n);
             n++;
             system("cls");
 		}
 		else if(menu==2)
 		{
-       		printf("----------------------------------------\n");
-			printf("           VIEW DATA PASIEN	\n");
-			printf("----------------------------------------\n");
+       		printf("\t========================================\n");
+			printf("\t          VIEW DATA PASIEN	\n");
+			printf("\t========================================\n");
             	if(n==0){
 					printf("\n Input Data Pasien Terlebih Dahulu!!!");
 				}else{
@@ -173,6 +294,23 @@ int main(){
 			system("cls");
 		}
 		else if(menu==3){
+			int cari_update;
+        		//system("cls");
+        	printf("\t========================================\n");
+			printf("\t          UPDATE DATA PASIEN	\n");
+			printf("\t========================================\n");
+        	printf("Masukan Id Pasien yang ingin di update : \n"); fflush(stdin); scanf("%d",& cari_update);
+        		
+        		int w;
+        		w=jumpsearch_idpasien(dt,n,cari_update);
+        		if(w==-1){
+        			getch();
+        			printf("\n DATA TIDAK DITEMUKAN");
+				}
+				else
+				{
+					update(w);
+				}
 		}
 		else if(menu==4){
 			do{
@@ -244,7 +382,88 @@ int main(){
 				}
 			}while(search!=4);
 		}
-			}while(menu!=0);
-			 printf("Inputan Salah");
+		else if(menu==5){
+			do{
+			//	system("cls");
+				pilihan_sort();
+				if(sort==1){
+        			printf("=======================================\n");
+        			printf("		SORT DATA PASIEN	           \n");
+        			printf("	   *Berdasarkan Nama*	           \n");
+        			printf("=======================================\n");
+					
+					for(i=0;i<=n;i++){
+						insertionsortnama(dt,n);
+					}for(i=0; i<n; i++){
+						view(i);								
+					}
+				}
+				else if(sort==2){
+					system("cls");
+        			printf("=======================================\n");
+        			printf("		SORT DATA PASIEN			   \n");
+        			printf("	*Berdasarkan ID Pasien*	           \n");
+        			printf("=======================================\n");
+					
+					for(i=0;i<=n;i++){
+					insertionshortid_pasien(dt,n);
+					}for(i=0; i<n; i++){
+						view(i);		
+					}	
+				}
+				else if(sort==3){
+					printf("=======================================\n");
+        			printf("		SORT DATA PASIEN	           \n");
+        			printf("	*Berdasarkan Jenis Penyakit*	   \n");
+        			printf("=======================================\n");
+
+					for(i=0;i<=n;i++){
+					insertionsortj_penyakit(dt,n);
+					}for(i=0;i<n; i++){
+						view(i);
+					}
+					
+				}
+				else if(sort==4){
+        			printf("=======================================\n");
+        			printf("		SORT DATA PASIEN               \n");
+        			printf("	*Berdasarkan Jenis Kamar*	       \n");
+        			printf("=======================================\n");
+					
+					for(i=0;i<=n;i++){
+					insertionsortj_kamar(dt,n);
+					}for(i=0; i<n; i++){
+						view(i);								
+					}
+				}
+				else if(sort==5){
+					printf("=======================================\n");
+        			printf("		SORT DATA PASIEN		       \n");
+        			printf("	*Berdasarkan Nomor Kamar*	       \n");
+        			printf("=======================================\n");
+					
+					for(i=0;i<=n;i++){
+					insertionshortno_kamar(dt,n);
+					}for(i=0;i<n;i++){
+						view(i);	
+					}
+				}
+				else if(sort==6){
+					printf("=======================================\n");
+        			printf("		SORT DATA PASIEN		       \n");
+        			printf("	*Berdasarkan Tanggal Masuk*	       \n");
+        			printf("=======================================\n");
+					
+					for(i=0;i<=n;i++){
+						insertionsorttgl_masuk(dt,n);
+					}for(i=0;i<n;i++){
+						view(i);	
+					}
+				}
+			}while(sort!=7);
+			
+			}
+		}while(menu!=0);
+		 printf("Inputan Salah");
 }
 
